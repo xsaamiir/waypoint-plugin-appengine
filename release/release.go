b9cb5e55811594c8a5cb175609dc88e5918139b6
samir elsharkawy <samir.elsharkawy@gmail.com>
2020-10-21 10:59:28 +0200

    Make release/release work

--- release/release.go
+++ release/release.go
@@ -2,29 +2,32 @@ package release
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
+	"google.golang.org/api/appengine/v1"
+
+	"github.com/sharkyze/waypoint-plugin-gae/internal/gae"
+	"github.com/sharkyze/waypoint-plugin-gae/platform"
 )
 
-type ReleaseConfig struct {
-	Active bool "hcl:directory,optional"
-}
+type ReleaseConfig struct{}
 
 type ReleaseManager struct {
 	config ReleaseConfig
 }
 
-// Implement Configurable
-func (rm *ReleaseManager) Config() interface{} {
-	return &rm.config
+// Config implements component.Configurable.
+func (rm *ReleaseManager) Config() (interface{}, error) {
+	return &rm.config, nil
 }
 
-// Implement ConfigurableNotify
+// ConfigSet implements component.ConfigurableNotify.
 func (rm *ReleaseManager) ConfigSet(config interface{}) error {
 	_, ok := config.(*ReleaseConfig)
 	if !ok {
-		// The Waypoint SDK should ensure this never gets hit
+		// The Waypoint SDK should ensure this never gets hit.
 		return fmt.Errorf("Expected *ReleaseConfig as parameter")
 	}
 
@@ -33,7 +36,7 @@ func (rm *ReleaseManager) ConfigSet(config interface{}) error {
 	return nil
 }
 
-// Implement Builder
+// ReleaseFunc implements component.ReleaseManager.
 func (rm *ReleaseManager) ReleaseFunc() interface{} {
 	// return a function which will be called by Waypoint
 	return rm.release
@@ -55,7 +58,7 @@ func (rm *ReleaseManager) ReleaseFunc() interface{} {
 // - hclog.Logger
 // - terminal.UI
 // - *component.LabelSet
-
+//
 // In addition to default input parameters the platform.Deployment from the Deploy step
 // can also be injected.
 //
@@ -66,10 +69,47 @@ func (rm *ReleaseManager) ReleaseFunc() interface{} {
 //
 // If an error is returned, Waypoint stops the execution flow and
 // returns an error to the user.
-func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI) (*Release, error) {
-	u := ui.Status()
-	defer u.Close()
-	u.Update("Release application")
+func (rm *ReleaseManager) release(
+	ctx context.Context,
+	deployment *platform.Deployment,
+	ui terminal.UI,
+) (*Release, error) {
+	st := ui.Status()
+	defer st.Close()
+
+	projectID := deployment.ProjectId
+	service := deployment.Service
+	versionID := deployment.VersionId
+
+	st.Update("Releasing App Engine version '" + versionID + "'")
+
+	appengineService, err := appengine.NewService(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	servicePatchCall := appengineService.Apps.Services.Patch(projectID, service, &appengine.Service{
+		Split: &appengine.TrafficSplit{Allocations: map[string]float64{versionID: 1}},
+	})
+	servicePatchCall.UpdateMask("split")
+	servicePatchCall = servicePatchCall.Context(ctx)
+
+	op, err := servicePatchCall.Do()
+	if err != nil {
+		return nil, err
+	}
+
+	op, err = gae.WaitForOperation(ctx, appengineService, op)
+	if err != nil {
+		return nil, err
+	}
+
+	if op.Error != nil {
+		st.Step(terminal.StatusError, "Traffic split error")
+		return nil, errors.New(op.Error.Message)
+	}
+
+	st.Step(terminal.StatusOK, "Traffic split successful")
 
 	return &Release{}, nil
 }
